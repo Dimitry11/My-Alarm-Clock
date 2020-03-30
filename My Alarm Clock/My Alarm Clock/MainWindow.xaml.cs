93aef5782ced24456bbbd21a4f703196d35be474
“Daniel <men2040@gmail.com>
2020-03-30 19:02:05 +0300

    some updates

---
+++ My Alarm Clock/My Alarm Clock/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace My_Alarm_Clock
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+    using System.Windows.Threading;
+
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromSeconds(0.1);
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            RotationSecond.Angle = 6 * DateTime.Now.Second;
+            RotationMinute.Angle = 6 * DateTime.Now.Minute;
+            RotationHour.Angle = 30 * DateTime.Now.Hour + 5 * (DateTime.Now.Minute / 10);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e) => this.Close();
+    }
+}
